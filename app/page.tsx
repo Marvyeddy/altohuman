59e04ebce07d04de5d7e0cc7461221a9c95fba8f
Marvelous Anyatonwu <anyatonwumarvelous32@gmail.com>
2026-05-10 18:43:07 +0100

    correction of alert component

--- app/page.tsx
+++ app/page.tsx
@@ -6,10 +6,17 @@ import Pricing from "@/components/home/Pricing";
 import Faq from "@/components/home/Faq";
 import Transform from "@/components/home/Transform";
 import Footer from "@/components/home/Footer";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 
 const HomePage = () => {
   return (
     <div className="w-full min-h-screen">
+      <Alert className="rounded-none bg-linear-to-r from-[#FF73C3] via-[#7B71FF] to-[#0EC9E2] flex items-center justify-center fixed border-none z-40">
+        <AlertDescription className="font-bold transition-all animate-pulse delay-1000 w-fit text-white max-md:text-xs">
+          Heads up! Altohuman is still in the testing phase...All features will
+          be fully functional soon.
+        </AlertDescription>
+      </Alert>
       <Hero />
       <Feature />
       <How />
